6a4ae7d40f28249b1015bf0ea8ef6be2f4cc1291
duan <duan@easya.cc>
2015-10-28 11:45:34 +0800

    *  user has many calendar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :teams, through: :memberships
   has_many :accesses
   has_many :projects, through: :accesses
+  has_many :calendars, through: :accesses
 
   has_secure_password
 end
