db67f8f24c138f98f51bd2e99cd9ecce9238312d
duan <duan@easya.cc>
2015-10-28 11:10:47 +0800

    *  team has_many calendars

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,6 @@
 class Team < ActiveRecord::Base
   has_many :projects
+  has_many :calendars
   has_many :memberships
   has_many :members, through: :memberships, class_name: 'User'
   has_many :events, as: :target
