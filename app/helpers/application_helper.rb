d5312902efdf3537f727bfa0d2488d55bff32369
duan <duan@easya.cc>
2015-10-28 17:08:26 +0800

    *  events#index 页面

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  include Rails.application.routes.url_helpers
+
   def current_user
     @user ||= User.create(name: 'test@mycolorway.com', password: 'foobar')
   end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,50 @@
 module EventsHelper
+  # 对 event 进行分组(日期、Projectable)
+  def events_group(events)
+    events.group_by { |event| [event_day_format(event.created_at.to_date), event.projectable] }
+  end
+
+  def event_day_format(date)
+    now = Time.now
+    case date_format(date)
+      when date_format(now)
+        '今'
+      when date_format(now - 1.days)
+        '昨'
+      else
+        date_format(date)
+    end
+  end
+
+  def date_format(date)
+    date.respond_to?(:strftime) ? date.strftime('%-m/%-d') : date.to_s
+  end
+
+  def projectable_url(projectable)
+    send("#{projectable.class.to_s.downcase}_url", target)
+  end
+
+  def target_name(target)
+    if target.respond_to?(:name)
+      target.name
+    elsif target.respond_to?(:title)
+      target.title
+    else
+      ''
+    end
+  end
+
+  def target_url(target)
+    send("#{target.class.to_s.downcase}_url", target)
+  end
+
+  def show_event_body(event)
+    if event.respond_to?(:comment) && event.comment.present?
+      "<a href='#{comment_url(event.comment)}'>#{event.comment.content}</a>"
+    elsif event.detail.present?
+      event.detail
+    else
+      ''
+    end
+  end
 end
