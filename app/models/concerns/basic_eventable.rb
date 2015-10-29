ee7ea220383de73a0a810de8f8f8ec43d5f096ef
duan <duan@easya.cc>
2015-10-29 14:25:30 +0800

    *  重构 trigger_event 方法来支撑 comment_event

--- app/models/concerns/basic_eventable.rb
+++ app/models/concerns/basic_eventable.rb
@@ -6,18 +6,19 @@ module Concerns
       has_many :events, as: :target
 
       after_create :trigger_created_event
-      after_destroy :trigger_deleted_event
+      before_destroy :trigger_deleted_event
     end
 
-    def trigger_event(initiator_id, action, action_params=[])
+    def trigger_event(initiator_id, action, action_params=[], type='Event')
       basic_params = {
           action: action_2_json(action, action_params),
-          type: 'Event',
+          type: type,
           initiator_id: initiator_id,
           target_id: self.id,
           target_type: self.class.to_s
-      }
-      Event.create(basic_params.merge!(projectable_params))
+      }.merge!(projectable_params)
+      yield(basic_params) if block_given? # 参数变化交由 block 来处理
+      Event.create(basic_params)
     end
 
     def action_2_json(action, action_params)
@@ -35,6 +36,8 @@ module Concerns
           {projectable_id: self.project.id, projectable_type: 'Project'}
         when CalendarEvent
           {projectable_id: self.caleventable.id, projectable_type: 'Calendar'}
+        when Comment
+          {projectable_id: self.commentable.project.id, projectable_type: 'Project'}
         else
           {}
       end
