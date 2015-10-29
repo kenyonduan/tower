95375ce19d6b9b9a60e7006efa50d747c0f34818
duan <duan@easya.cc>
2015-10-29 14:26:06 +0800

    *  完成创建和删除 comment 的 event

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,6 @@
 #  content          :text
 #  commentable_id   :integer
 #  commentable_type :string
-#  event_id         :integer
 #  creator_id       :integer
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
@@ -14,28 +13,29 @@
 
 class Comment < ActiveRecord::Base
   include Concerns::BasicEventable
+
   has_many :comment_events, through: :events
   # 多种类型的资源都有 Comment
   belongs_to :commentable, polymorphic: true
   belongs_to :creator, class_name: 'User'
 
-  validates :content, :event_id, :commentable_type, :commentable_id, presence: true
+  validates :content, :commentable_type, :commentable_id, presence: true
   validates :commentable_id, numericality: {only_integer: true, greater_than: 0}
 
-  after_create :trigger_created_event
-  after_destroy :trigger_deleted_event
-
-  def trigger_created_event
-
+  def trigger_created_event(by)
+    trigger_event(by, "回复了#{commentable_name}", [], 'CommentEvent') { |basic_params| basic_params.merge!(comment_id: self.id) }
   end
 
-  def trigger_deleted_event
-
+  def trigger_deleted_event(by)
+    trigger_event(by, '删除了回复', [], 'CommentEvent') { |basic_params| basic_params.merge!(detail: self.content) }
   end
 
-  def trigger_comment_event(initiator_id, action, action_params=[])
-    CommentEvent.create(
-
-    )
+  def commentable_name
+    case self.commentable
+      when Todo
+        '任务'
+      else
+        'Unkonw'
+    end
   end
 end
