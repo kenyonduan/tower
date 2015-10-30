a2aaf66fc2f23fec1458bbcf4d15c899a955f8a6
Kenyon Duan <kenyonduan@gmail.com>
2015-10-30 23:10:44 +0800

    *  EventsController rspec test

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,13 @@
 class EventsController < ApplicationController
+  before_action :find_team, only: :index
+
   def index
-    @project_ids = current_user.projects.pluck(:id)
-    @calendar_ids = current_user.calendars.pluck(:id)
-    @events = EventForm.new(p_ids: @project_ids, c_ids: @calendar_ids, page: params[:page]).query
+    project_ids, calendar_ids = current_user.accessible_projects_id(@team), current_user.accessible_calendars_id(@team)
+    @events = EventForm.new(p_ids: project_ids, c_ids: calendar_ids, page: params[:page]).query
+  end
+
+  private
+  def find_team
+    @team = Team.find(params[:team_id])
   end
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
+  before(:all) { init_feeds }
+  before(:each) { set_user_session(@user) }
 
+  it 'populates an array of events' do
+    get :index, team_id: @team.id
+    expect(assigns(:events)).to match_array(@todo.events + @todo_list.events)
+    expect(response).to render_template :index
+  end
+
+  it 'visit by new user' do
+    user = create(:user)
+    team = create(:team, creator: user)
+
+    get :index, team_id: team.id
+    expect(assigns(:events)).to be_blank
+    expect(response).to render_template :index
+  end
 end
