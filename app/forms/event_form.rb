2e79fb772153962295130d0aefc38269cbd9c16e
Kenyon Duan <kenyonduan@gmail.com>
2015-10-30 23:11:03 +0800

    *  删除无用的 includes

--- app/forms/event_form.rb
+++ app/forms/event_form.rb
@@ -1,4 +1,5 @@
 class EventForm
+  include ActiveModel::Model
   attr_accessor :p_ids, :c_ids, :page
 
   def p_ids
@@ -15,10 +16,10 @@ class EventForm
 
   # TODO 如果采取全局 uuid 是否就不用传递 projectable_type 了?
   def query
-    Event.includes(:target, :initiator).where(
+    Event.where(
         '(projectable_type=? and projectable_id IN(?)) OR (projectable_type=? and projectable_id IN(?))',
         'Project', p_ids,
         'Calendar', c_ids
-    ).order(created_at: :desc).per(50).page(page)
+    ).order(created_at: :desc).page(page).per(50)
   end
 end
