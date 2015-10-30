2bba95fd07d2f5b0c6b0d9254e3fed620281117c
Kenyon Duan <kenyonduan@gmail.com>
2015-10-31 01:26:19 +0800

    *  TodosController rspec test

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,7 @@
 class TodosController < ApplicationController
+  before_action :find_project
   before_action :check_project_access, only: :create
-  before_action :find_todo, except: :create
-  before_action :check_todo_access, except: :create
+  before_action :find_todo, :check_todo_access, except: :create
 
   def create
     todo = Todo.create(todo_params.merge(creator: current_user, todo_list_id: params[:todo_list_id]))
@@ -51,13 +51,13 @@ class TodosController < ApplicationController
   end
 
   def check_todo_access
-    if current_user.id != @todo.creator_id # 创建者就一定有权限
+    if current_user != @todo.creator # 创建者就一定有权限
       render json: permission_denied_resp unless current_user.accessible_projects_id(@project.team).include?(@todo.todo_list.project_id)
     end
   end
 
   def check_project_access
-    unless current_user.accessible_projects_id(@project.team).include?(params[:project_id])
+    unless current_user.accessible_projects_id(@project.team).include?(@project.id)
       render json: permission_denied_resp
     end
   end
--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe TodosController, type: :controller do
+  before(:all) { init_feeds }
+  before(:each) { set_user_session(@user) }
+  let(:actions) { %w(destroy finish assign reassign reschedule) }
 
+  describe '#create' do
+    it 'success create' do
+      post :create, project_id: @project.id, todo_list_id: @todo_list.id, todo: {title: 'foobar'}
+      todo = JSON.parse(response.body)
+      expect(todo['todo_list_id']).to eq(@todo_list.id)
+      expect(todo['title']).to eq('foobar')
+    end
+
+    it 'failed create' do
+      @user.accesses.clear
+      post :create, project_id: @project.id, todo_list_id: @todo_list.id, todo: {title: 'foobar'}
+      expect(JSON.parse(response.body)).to eq(permission_denied_resp)
+    end
+  end
+
+  describe '#actions' do
+    it 'response success' do
+      actions.each do |action|
+        post action.to_sym, project_id: @project.id, todo_list_id: @todo_list.id, id: @todo.id, assignee_id: @user.id
+        expect(JSON.parse(response.body)).to eq(success_resp)
+      end
+    end
+
+    it 'response permission denied' do
+      project = create(:project, creator: @user, team: @team, project_type: Project.project_types[:standard])
+      todo_list = create(:todo_list, creator: @user, project: project, list_type: TodoList.list_types[:defalut])
+      todo = create(:todo, creator: create(:user), todo_list: todo_list)
+
+      actions.each do |action|
+        post action.to_sym, project_id: @project.id, todo_list_id: @todo_list.id, id: todo.id
+        expect(JSON.parse(response.body)).to eq(permission_denied_resp)
+      end
+    end
+  end
 end
