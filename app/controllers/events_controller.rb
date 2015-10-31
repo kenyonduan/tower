8b6666e483d64900806cdf353cb5beb82e08266a
Kenyon Duan <kenyonduan@gmail.com>
2015-10-31 19:24:26 +0800

    *  根据请求类型返回不同页面

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,10 +4,15 @@ class EventsController < ApplicationController
   def index
     project_ids, calendar_ids = current_user.accessible_projects_id(@team), current_user.accessible_calendars_id(@team)
     @events = EventForm.new(p_ids: project_ids, c_ids: calendar_ids, page: params[:page]).query
+    render layout: false if @is_ajax
   end
 
   private
   def find_team
     @team = Team.find(params[:team_id])
+  end
+
+  def is_ajax_request?
+    @is_ajax = request.xhr?
   end
 end
