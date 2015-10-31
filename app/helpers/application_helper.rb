4eb48f6469a21c5053d7a0c63bdd392e19fa3db2
Kenyon Duan <kenyonduan@gmail.com>
2015-10-31 19:23:16 +0800

    *  将 team_id 存储在 session 中

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,11 @@ module ApplicationHelper
     @current_user ||= User.find(session[:user])
   end
 
+  # TODO team 切换时需要更新数据，暂时先快速包容一下
+  def current_team_id
+    session[:team]
+  end
+
   def url_helpers
     @url_helpers ||= Rails.application.routes.url_helpers
   end
